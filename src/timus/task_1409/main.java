85caeb66a4126fc508f9fba7a0a4d0badfa48891
Nikolay Tolstykh <magistr666@gmail.com>
2022-12-05 13:44:55 +0500

    Urfu_ex 05.12

---
+++ src/timus/task_1409/main.java
@@ -0,0 +1,15 @@
+package timus.task_1409;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = a + b - 1;
+        System.out.println(c-a);
+        System.out.println(c-b);
+        in.close();
+    }
+}
