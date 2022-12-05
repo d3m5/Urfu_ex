47843cc51f2a2740bfa942e0683b4ce76dd79b89
Nikolay Tolstykh <magistr666@gmail.com>
2022-12-05 12:25:24 +0500

    Urfu_ex 05.12

---
+++ src/lr2/Example3.java
@@ -0,0 +1,18 @@
+package lr2;
+
+import java.util.Scanner;
+
+public class Example3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Введите цисло А которое делится на 4 и больльше 10: ");
+        float a = in.nextInt();
+        if (a % 4 == 0 && a >= 10) {
+            System.out.print("Успешно A делится на 4 и больше 10");
+        } else {
+            System.out.print("Не успешно, попробуйте еще");
+
+        }
+        in.close();
+    }
+}
